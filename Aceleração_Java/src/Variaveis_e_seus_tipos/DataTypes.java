1ba1b0460b880adabb735214a9b9752ad4ea77a2
Lucas Israel <lisraelalves92@gmail.com>
2023-08-04 09:51:22 -0300

    float number

--- Aceleração_Java/src/Variaveis_e_seus_tipos/DataTypes.java
+++ Aceleração_Java/src/Variaveis_e_seus_tipos/DataTypes.java
@@ -10,6 +10,8 @@ public class DataTypes {
 	static int pInt2 = 5000;
 	static int pInt3 = 4000;
 	static long pLong = 9_223_372_036_854_775_807L;
+	static float pFloat1 = -101.23f;
+	static float pFloat2 = 2.356f;
 	
 	
     public static void main(String[] args) {
@@ -20,6 +22,8 @@ public class DataTypes {
         		DataTypes.typeInt(pInt),
         		DataTypes.intSum(pInt2, pInt3),
         		DataTypes.typeLong(pLong),
+        		DataTypes.typeFloat(pFloat1),
+        		DataTypes.typeFloat(pFloat2),
         };
         DataTypes.loopPrint(newArr);
         DataTypes.inlineSum();
@@ -32,21 +36,35 @@ public class DataTypes {
     }
     
 //    Exercicios sobre como definir metodos.
+	
+//	  byte
     
     public static byte typeByte(byte number) {
     	return number;
     }
     
+//    short
+    
     public static short typeShort(short number) {
     	return number;
     }
+    
+//    int
     
     public static int typeInt(int number) {
     	return number;
     }
     
+//    long
+    
     public static long typeLong(long number) {
     	return number;
+    }
+    
+//    float
+    
+    public static String typeFloat(float number) {
+    	return "Float number " + number;
     }
     
     
@@ -56,10 +74,13 @@ public class DataTypes {
     	return num1 + num2;
     }
     
+//    Declaração inline.
+    
     public static void inlineSum() {
     	int numeroInteiro = 20;
     	int a = 4, b = 6, soma;
     	soma = a + b;
+    	System.out.println("------- Inline Sum ----------");
     	System.out.println("Numero Inteiro " + numeroInteiro);
     	System.out.println("Somando " + soma);
     }
