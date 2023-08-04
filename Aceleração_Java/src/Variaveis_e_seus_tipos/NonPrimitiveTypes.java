51be22a3057fd3948c906402759d41b04ece1ed6
Lucas Israel <lisraelalves92@gmail.com>
2023-08-04 10:37:10 -0300

    string type

---
+++ Aceleração_Java/src/Variaveis_e_seus_tipos/NonPrimitiveTypes.java
@@ -0,0 +1,40 @@
+package Variaveis_e_seus_tipos;
+
+public class NonPrimitiveTypes {
+	
+	static String str1 = "Isso é uma string"; //precisa ser aspas duplas para indicar string
+	static String str2 = "Isso é outra string";
+
+	public static void main(String[] args) {
+		Object[] newArr = {
+				str1,
+				str2,
+				str1 +", "+ str2,
+				NonPrimitiveTypes.stringBuilder(str1, str2, "abcde", "fghi"),
+		};
+		
+		NonPrimitiveTypes.printer(newArr);
+	}
+	
+//	printer
+	
+	public static void printer(Object[] args) {
+		for(Object arg : args) {
+			System.out.println(arg);
+		}
+	}
+	
+//	stringBuilder
+	
+	public static String stringBuilder(String... args) {
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for (String arg : args) {
+			stringBuilder.append(arg).append(" ");
+		}
+		
+		return stringBuilder.toString();
+	}
+	
+}
